d91f6e116ff00615edf13be23229590cb92e2613
AAKASH SAMADHAN JADHAV <aakashjadhav792@gmail.com>
2026-02-07 13:07:28 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,14 @@ const Hero = () => {
   return (
     <section
       className="flex items-center justify-center
-                 pt-28 pb-20 md:min-h-screen
+                 min-h-[100svh]
+                 pt-24 pb-16
                  bg-gradient-to-br from-[#020617] via-[#0F172A] to-[#020617]"
     >
-      <div className="text-center px-6">
-
+      <div
+        className="text-center px-6 max-w-3xl mx-auto
+                   -translate-y-10 md:translate-y-0"
+      >
         <p className="text-muted text-base md:text-3xl mb-3 tracking-wide">
           Aakash Jadhav
         </p>
@@ -39,15 +42,12 @@ const Hero = () => {
             target="_blank"
             rel="noreferrer"
             className="px-6 py-2 rounded-lg font-medium
-             border border-[#1F2933] text-white
-             hover:border-[#2563EB] transition"
+                       border border-[#1F2933] text-white
+                       hover:border-[#2563EB] transition"
           >
             View Resume
           </a>
-
-
         </div>
-
       </div>
     </section>
   );
